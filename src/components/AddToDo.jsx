0c514f4b9ee1fae1a3c56a7dd27316561c1d2de4
Noel <noeltesdev@gmail.com>
2025-05-06 16:02:47 -0600

    tailwindcss update

--- src/components/AddToDo.jsx
+++ src/components/AddToDo.jsx
@@ -29,16 +29,16 @@ export const AddToDo = ({todos,fetchData}) => {
   return (
     <>
    
-    <div className=' p-8 bg-stone-200 mt-2 w-1/2 m-auto  text-center'>
-        <form className='bg-stone-400 p-3  flex-column justify-center items-center space-y-4' onSubmit={handleSubmit}> 
-            <h2 className=' text-center text-2xl text-stone-50'>Add As many todo's as you wish</h2>
+    <div className="p-4 sm:p-8 bg-stone-200 mt-2 w-full sm:w-1/2 mx-auto text-center">
+        <form className="bg-stone-400 p-4 flex flex-col justify-center items-center space-y-4" onSubmit={handleSubmit}> 
+            <h2 className="text-center text-xl sm:text-2xl text-stone-50">Add As many todo's as you wish</h2>
 
-            <div className='mt-2'>
+            <div className="w-full flex flex-col sm:flex-row items-center mt-2 space-y-2 sm:space-y-0 sm:space-x-2">
             <label htmlFor="todoname" className= ' font-bold'>what to do</label>
-            <input type="text" name="todoname" id="todoname" placeholder='Enter todo here' className=' bg-stone-50 p-2 rounded ml-2' value={todo} onChange={(e)=>setToDo(e.target.value)}/>
+            <input type="text" name="todoname" id="todoname" placeholder='Enter todo here' className="bg-stone-50 p-2 rounded w-full sm:w-auto" value={todo} onChange={(e)=>setToDo(e.target.value)}/>
             </div>
             
-            <button className=' bg-blue-900  py-2 px-4 rounded-lg w-50   m-auto text-white hover:cursor-pointer hover:bg-blue-600' type='submit'>AddToDo</button>
+            <button className="bg-blue-900 py-2 px-4 rounded-lg w-full sm:w-auto text-white hover:cursor-pointer hover:bg-blue-600" type='submit'>AddToDo</button>
             </form>
            
             </div>
