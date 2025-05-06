e7bbb23bf75a3c8eef249768842a341b8df4a601
Noel <noeltesdev@gmail.com>
2025-05-06 16:06:57 -0600

    tailwindcss update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Button = ({label,onClick}) => {
   return (
     <>
-        <button className='  bg-blue-800 rounded px-3 py-1 text-stone-50 hover:cursor-pointer hover:bg-blue-500' onClick={onClick}>{label}</button>
+        <button className="bg-blue-800 rounded px-4 py-2 text-stone-50 text-sm sm:text-base w-full sm:w-auto hover:cursor-pointer hover:bg-blue-500" onClick={onClick}>{label}</button>
     </>
   )
 }
