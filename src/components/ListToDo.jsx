2c10af252718cd84720580cc5de3deee012f1881
Noel <noeltesdev@gmail.com>
2025-05-06 15:41:24 -0600

    updated delete method

--- src/components/ListToDo.jsx
+++ src/components/ListToDo.jsx
@@ -1,13 +1,11 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import done from  '../assets/done.png';
-import del from  '../assets/delete.png';
-import edit from '../assets/edit.png'
-import save from '../assets/save.png'
-import clear from '../assets/clear.png'
+import done from '../assets/done.png';
+import del from '../assets/delete.png';
+import edit from '../assets/edit.png';
+import save from '../assets/save.png';
+import clear from '../assets/clear.png';
 import { FilterTasks } from './FilterTasks';
-
-
 
 export const ListToDo = ({ todos, fetchData }) => {
   const [editNoteId, setEditNoteId] = useState(null);
@@ -17,7 +15,7 @@ export const ListToDo = ({ todos, fetchData }) => {
   const handleDelete = (id) => {
     //delete an item with a specific id from the database
     axios
-      .delete(`http://localhost:5000/api/todos/${id}`)
+      .delete(`https://todoapp-backend-900w.onrender.com/api/todos/${id}`)
       .then((response) => {
         alert('Deleted:' + JSON.stringify(response.data.message));
         //refresh the ui
@@ -29,14 +27,14 @@ export const ListToDo = ({ todos, fetchData }) => {
   };
 
   const handleEdit = (id) => {
-
-    const todo = todos.find(t=>t._id===id)
+    const todo = todos.find((t) => t._id === id);
     //toggle between Save and Edit
     if (editNoteId === id) {
-      
       //handle save logic here
       axios
-        .put(`https://todoapp-backend-900w.onrender.com/api/todos/${id}`, { text: editedItemsNote[id] })
+        .put(`https://todoapp-backend-900w.onrender.com/api/todos/${id}`, {
+          text: editedItemsNote[id],
+        })
         .then((response) => {
           alert('Saved' + JSON.stringify(response.data.message));
           fetchData('all');
@@ -47,82 +45,113 @@ export const ListToDo = ({ todos, fetchData }) => {
       setEditNoteId(null);
     } else {
       setEditNoteId(id);
-      setEditedItemsNote(prev=>({...prev,[id]:todo.text}));
+      setEditedItemsNote((prev) => ({ ...prev, [id]: todo.text }));
     }
   };
 
   const handleCheckbox = (e, todoid) => {
-    const isChecked = e.target.checked
+    const isChecked = e.target.checked;
     setCheckedItems((prev) => ({ ...prev, [todoid]: isChecked }));
 
-      //handle update logic
-      axios.put(`https://todoapp-backend-900w.onrender.com/api/todos/${todoid}`,{completed:isChecked})
-      .then(()=>{
+    //handle update logic
+    axios
+      .put(`https://todoapp-backend-900w.onrender.com/api/todos/${todoid}`, {
+        completed: isChecked,
+      })
+      .then(() => {
         fetchData('all');
       })
-      .catch((error)=>{
-        console.error('error',error)
-      })
-
+      .catch((error) => {
+        console.error('error', error);
+      });
   };
 
-  const clearAllTasks = ()=>{
-    axios.delete('https://todoapp-backend-900w.onrender.com/api/todos/')
-    .then((response)=>{
-       console.log(response.data)
-       fetchData('all');// refresh your list
-    })
-    .catch((error)=>{
-      console.error(error)
-    })
-  }
+  const clearAllTasks = () => {
+    axios
+      .delete('https://todoapp-backend-900w.onrender.com/api/todos/')
+      .then((response) => {
+        console.log(response.data);
+        fetchData('all'); // refresh your list
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   return (
     <div className=" mt-3  px-8 py-3 bg-blue-400">
       <h2 className=" text-2xl font-semibold text-center flex justify-between items-center  pb-2">
-        <span>ToDo List (mark the checkbox if task complete) </span> <FilterTasks  fetchData={fetchData}/> <span className=' ml-2.5 rounded bg-red-400'><img src={clear} alt="clear icon"  className=' w-10 h-10 object-cover rounded hover:cursor-pointer' onClick={clearAllTasks}/></span>
+        <span>ToDo List (mark the checkbox if task complete) </span>{' '}
+        <FilterTasks fetchData={fetchData} />{' '}
+        <span className=" ml-2.5 rounded bg-red-400">
+          <img
+            src={clear}
+            alt="clear icon"
+            className=" w-10 h-10 object-cover rounded hover:cursor-pointer"
+            onClick={clearAllTasks}
+          />
+        </span>
       </h2>
 
-        {todos.map((todo) => (
-          <div
-            key={todo._id}
-            className={`flex justify-between items-center text-white  mb-2 p-2 rounded shadow  
-              ${
-              todo.completed ? 'bg-green-600' : 'bg-stone-600'
-            }`}
-          >
-            {editNoteId === todo._id ? (
-              <input
-                type="text"
-                value={editedItemsNote[todo._id]}
-                onChange={(e) => setEditedItemsNote(prev=>({
+      {todos.map((todo) => (
+        <div
+          key={todo._id}
+          className={`flex justify-between items-center text-white  mb-2 p-2 rounded shadow  
+              ${todo.completed ? 'bg-green-600' : 'bg-stone-600'}`}
+        >
+          {editNoteId === todo._id ? (
+            <input
+              type="text"
+              value={editedItemsNote[todo._id]}
+              onChange={(e) =>
+                setEditedItemsNote((prev) => ({
                   ...prev,
-                [todo._id]:e.target.value
-                }))}
-                className=' w-full rounded  p-2 mr-1 bg-black '
+                  [todo._id]: e.target.value,
+                }))
+              }
+              className=" w-full rounded  p-2 mr-1 bg-black "
+            />
+          ) : (
+            <span>{todo.text}</span>
+          )}
+          {!todo.completed ? (
+            <div className=" flex gap-2">
+              <img
+                onClick={() => handleDelete(todo._id)}
+                src={del}
+                alt="delete icon"
+                className=" w-8 h-8  object-cover rounded hover:cursor-pointer"
               />
-            ) : (
-              <span>{todo.text}</span>
-            )}
-            {!todo.completed ? (
-              <div className=" flex gap-2">
 
-                  <img onClick={() => handleDelete(todo._id)} src={del} alt="delete icon" className=' w-8 h-8  object-cover rounded hover:cursor-pointer' />
-
-                  
-                  {editNoteId === todo._id ? <img  onClick={() => handleEdit(todo._id)} src={save} alt="save icon" className=' w-8 h-8  object-cover rounded hover:cursor-pointer' /> : <img  onClick={() => handleEdit(todo._id)} src={edit} alt="edit icon" className=' w-8 h-8 object-cover rounded hover:cursor-pointer' />}
-                <input
-                  type="checkbox"
-                  name=""
-                  id=""
-                  onChange={(e) => handleCheckbox(e, todo._id)}
-                  checked={checkedItems[todo._id] || false}
-                  className=' hover:cursor-pointer'
+              {editNoteId === todo._id ? (
+                <img
+                  onClick={() => handleEdit(todo._id)}
+                  src={save}
+                  alt="save icon"
+                  className=" w-8 h-8  object-cover rounded hover:cursor-pointer"
                 />
-              </div>
-            ):<img src={done} className='w-6' alt='done icon'/>}
-          </div>
-        ))}
+              ) : (
+                <img
+                  onClick={() => handleEdit(todo._id)}
+                  src={edit}
+                  alt="edit icon"
+                  className=" w-8 h-8 object-cover rounded hover:cursor-pointer"
+                />
+              )}
+              <input
+                type="checkbox"
+                name=""
+                id=""
+                onChange={(e) => handleCheckbox(e, todo._id)}
+                checked={checkedItems[todo._id] || false}
+                className=" hover:cursor-pointer"
+              />
+            </div>
+          ) : (
+            <img src={done} className="w-6" alt="done icon" />
+          )}
+        </div>
+      ))}
     </div>
   );
 };
