82425c79dbaa153ff6f6c944f41621b07bb871e4
Noel <noeltesdev@gmail.com>
2025-05-15 09:44:24 -0600

    modified frontend

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import { FilterTasks } from './FilterTasks';
 export const Header = () => {
   // const { message } = useContext(AuthContext);
   return (
-<div className='fixed top-0 left-0 right-0 z-50 w-full h-16 flex  py-2 px-2 bg-gr'>
+<div className='fixed top-0 left-0 right-0 z-50 w-full h-16 flex  py-2 px-2'>
 <FilterTasks/>
 <span className=' self-center ml-auto font-buttons text-2xl text-stone-50 '>Logout</span>
 </div>
