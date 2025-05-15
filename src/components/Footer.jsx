3c0618d6747336cf96c69ee6eb7941e637edd7e1
Noel <noeltesdev@gmail.com>
2025-05-15 11:04:39 -0600

    modified frontend

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <p className=' text-base sm:text-lg md:text-xl lg:text-2xl text-center bg-stone-400 py-2 text-white font-buttons'>
+      <p className=' text-base sm:text-lg md:text-xl lg:text-2xl text-center py-1 text-white font-buttons'>
         &copy;2025 by Noel Tesfe All rights reserved.
       </p>
     </>
