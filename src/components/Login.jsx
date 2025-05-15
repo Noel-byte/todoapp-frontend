9f96963b28c61e837190fac724ff71371f5c2b49
Noel <noeltesdev@gmail.com>
2025-05-15 09:19:49 -0600

    modified frontend

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -47,11 +47,11 @@ export const Login = () => {
   return (
     <div className="relative h-screen w-screen">
       <div className=" fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white/10 backdrop-blur-md border border-white/20 rounded-lg shadow  p-6 w-full max-w-md space-y-3 text-center text-white">
-        <h1 className="text-left font-titles px-1 py-2 text-orangered text-base sm:text-2xl md:text-3xl lg:text-4xl">
+        <h1 className="text-left font-titles px-1 py-2 text-heading text-base sm:text-2xl md:text-3xl lg:text-4xl">
           Todo - App
         </h1>
         <div className="bg-gray-200 h-px shadow-lg"></div>
-        <h2 className=" text-base sm:text-xl md:text-2xl lg:text-3xl font-titles text-orangered">
+        <h2 className=" text-base sm:text-2xl md:text-3xl lg:text-4xl font-titles text-heading">
           Login to your account
         </h2>
         <div className=" flex  gap-2 items-center justify-center ">
@@ -61,7 +61,7 @@ export const Login = () => {
           <span>
             <NavLink
               to="/register"
-              className=" text-base sm:text-lg md:text-xl lg:text-3xl font-semibold text-blue-700 font-buttons hover:text-blue-500"
+              className=" text-base sm:text-lg md:text-xl lg:text-3xl font-semibold text-signup font-buttons hover:text-4xl"
             >
               SignUp
             </NavLink>
@@ -97,7 +97,7 @@ export const Login = () => {
           />
           <button
             type="submit"
-            className="bg-blue-900 py-2 px-12 font-buttons text-base sm:text-lg md:text-xl lg:text-2xl rounded-lg w-full sm:w-auto text-white hover:cursor-pointer hover:bg-blue-600"
+            className="bg-button py-2 px-12 font-buttons text-base sm:text-lg md:text-xl lg:text-2xl rounded-lg w-full sm:w-auto text-white hover:cursor-pointer hover:bg-button/10"
           >
             Login
           </button>
