1f4a79a6781f4b5dbb0d0a4851ba591024f8d467
Noel <noeltesdev@gmail.com>
2025-05-16 16:56:02 -0600

    modified frontend

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -47,7 +47,7 @@ export const Login = () => {
   return (
     <div className="relative h-screen w-screen">
       <div className=" fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white/10 backdrop-blur-md border border-white/20 rounded-lg shadow  p-6 w-9/10 max-w-md space-y-3 text-center text-white">
-        <h1 className="text-left font-titles px-1 py-2 text-heading text-base sm:text-2xl md:text-3xl lg:text-4xl">
+        <h1 className="text-left font-titles px-1 py-2 text-heading text-4xl">
           Todo - App
         </h1>
         <div className="bg-gray-200 h-px shadow-lg"></div>
@@ -61,7 +61,7 @@ export const Login = () => {
           <span>
             <NavLink
               to="/register"
-              className=" text-base sm:text-lg md:text-xl lg:text-3xl font-semibold text-signup font-buttons hover:text-signup/60"
+              className="text-3xl font-semibold text-signup font-buttons hover:text-signup/60"
             >
               SignUp
             </NavLink>
