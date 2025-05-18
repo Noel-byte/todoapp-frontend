f258945f05d397553952a151f174daf03cdd14ab
Noel <noeltesdev@gmail.com>
2025-05-17 20:00:32 -0600

    modified frontend

--- src/components/FilterTasks.jsx
+++ src/components/FilterTasks.jsx
@@ -85,21 +85,21 @@ export const FilterTasks = () => {
           <>
             <span
               onClick={allTasks}
-              className=" font-buttons bg-white/10  px-4 py-1 rounded  w-full hover:cursor-pointer"
+              className=" font-buttons   px-4 py-1 rounded  w-full hover:cursor-pointer"
             >
               All Tasks
             </span>
 
             <span
               onClick={completedTasks}
-              className=" font-buttons bg-white/10  px-4 py-1 rounded w-full hover:cursor-pointer "
+              className=" font-buttons  px-4 py-1 rounded w-full hover:cursor-pointer "
             >
               Completed
             </span>
 
             <span
               onClick={inCompleteTasks}
-              className=" font-buttons bg-white/10  px-4 py-1 rounded  w-full hover:cursor-pointer "
+              className=" font-buttons   px-4 py-1 rounded  w-full hover:cursor-pointer "
             >
               Incomplete
             </span>
@@ -107,7 +107,7 @@ export const FilterTasks = () => {
             <span
               // className="bg-blue-900 py-2 px-4 rounded-lg text-white hover:cursor-pointer hover:bg-blue-600 w-full"
               onClick={() => clearAllTasks(todos[0]?.user)}
-              className=" font-buttons bg-white/10 text-red-600 px-4 py-1 rounded  w-full hover:cursor-pointer"
+              className=" font-buttons  text-red-800 px-4 py-1 rounded  w-full hover:cursor-pointer"
             >
               Clear
             </span>
