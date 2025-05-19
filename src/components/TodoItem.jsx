f3b5ad58529a2ffa62abbc07613b7dd356c077ae
Noel <noeltesdev@gmail.com>
2025-05-19 09:03:46 -0600

    modified frontend

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -23,7 +23,7 @@ export const TodoItem = ({ todo }) => {
     const handleScroll = () => {
       if (taskRef.current) {
         const rect = taskRef.current.getBoundingClientRect();
-        const triggerPoint = window.innerHeight * 0.4; //40% down the screen
+        const triggerPoint = window.innerHeight * 0.2; //40% down the screen
 
         if (rect.top < triggerPoint && rect.bottom > 0) {
           setIsVisible(true);
