fe9b2807668b44de4dff2c1e61b588ee33a3da51
Noel <noeltesdev@gmail.com>
2025-05-19 07:15:06 -0600

    modified frontend

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Heading = (title) => {
+export const Heading = ({title}) => {
   return (
      <h1 className="text-left font-titles px-1 py-2 text-emerald text-base sm:text-2xl md:text-3xl lg:text-4xl">
           {title}
